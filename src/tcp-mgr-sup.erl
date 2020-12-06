9af68ce859bf5d3bfe555a76c508cd2452fc981c
Duncan McGreggor <oubiwann@gmail.com>
2020-12-06 01:26:58 -0600

    Basically rewrote it. Gonna have to un-fork,

--- src/tcp-mgr-sup.erl
+++ src/tcp-mgr-sup.erl
@@ -1,30 +1,3 @@
-%% -------------------------------------------------------------------
-%%
-%% Copyright (c) 2015 Carlos Andres Bolaños, Inc. All Rights Reserved.
-%%
-%% This file is provided to you under the Apache License,
-%% Version 2.0 (the "License"); you may not use this file
-%% except in compliance with the License.  You may obtain
-%% a copy of the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing,
-%% software distributed under the License is distributed on an
-%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
-%% specific language governing permissions and limitations
-%% under the License.
-%%
-%% -------------------------------------------------------------------
-
-%%%-------------------------------------------------------------------
-%%% @author Carlos Andres Bolaños R.A. <cabol.dev@gmail.com>
-%%% @copyright (C) 2015, <Carlos Andres Bolaños>, All Rights Reserved.
-%%% @doc
-%%% Supervisor.
-%%% @end
-%%%-------------------------------------------------------------------
 -module('tcp-mgr-sup').
 
 -behaviour(supervisor).
@@ -35,23 +8,20 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(Name, Size, Address, Port, Options),
-  {
-   Name,
-   {tcp_client, start_link, [Name, Size, Address, Port, Options]},
-   permanent,
-   5000,
-   worker,
-   [tcp_client]
-  }
-).
+-define(MGR, 'tcp-mgr-socket').
+-define(CHILD(Host, Port, Options),
+  #{id => ?MGR,
+    start => {?MGR, start_link, [Host, Port, Options]},
+    restart => permanent,
+    shutdown => 5000,
+    type => worker,
+    modules => [?MGR]
+   }).
 
 %%%===================================================================
 %%% API functions
 %%%===================================================================
 
--spec start_link() -> supervisor:startlink_ret().
 start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
@@ -59,14 +29,13 @@ start_link() ->
 %%% Supervisor callbacks
 %%%===================================================================
 
-%% @hidden
 init([]) ->
-  {ok, Pools} = application:get_env(tcp_client, pools),
-  Fun = fun({Name, Size, Options}) ->
-          {_, Address} = lists:keyfind(address, 1, Options),
-          {_, Port} = lists:keyfind(port, 1, Options),
-          {_, Opts} = lists:keyfind(options, 1, Options),
-          ?CHILD(Name, Size, Address, Port, Opts)
-        end,
-  Children = lists:map(Fun, Pools),
-  {ok, {{one_for_one, 5, 10}, Children}}.
+  {ok, Server} = application:get_env('tcp-mgr', server),
+  {host, Host} = lists:keyfind(host, 1, Server),
+  {port, Port} = lists:keyfind(port, 1, Server),
+  {options, Options} = lists:keyfind(options, 1, Server),
+  SupFlags = #{strategy => one_for_one,
+               intensity => 5,
+               period => 10},
+  Child = ?CHILD(Host, Port, Options),
+  {ok, {SupFlags, [Child]}}.
--- src/tcp-mgr.erl
+++ src/tcp-mgr.erl
@@ -1,95 +1,16 @@
-%% -------------------------------------------------------------------
-%%
-%% Copyright (c) 2015 Carlos Andres Bolaños, Inc. All Rights Reserved.
-%%
-%% This file is provided to you under the Apache License,
-%% Version 2.0 (the "License"); you may not use this file
-%% except in compliance with the License.  You may obtain
-%% a copy of the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing,
-%% software distributed under the License is distributed on an
-%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
-%% specific language governing permissions and limitations
-%% under the License.
-%%
-%% -------------------------------------------------------------------
-
-%%%-------------------------------------------------------------------
-%%% @author Carlos Andres Bolaños R.A. <cabol.dev@gmail.com>
-%%% @copyright (C) 2015, <Carlos Andres Bolaños>, All Rights Reserved.
-%%% @doc Main interface.
-%%%-------------------------------------------------------------------
 -module('tcp-mgr').
 
 %% API
--export([start_link/4, start_link/5]).
--export([sync_send/2, sync_send/3]).
--export([send/2, send/3]).
+-export([call_msg/1, cast_msg/1]).
+
+-define(MGR, {global, 'tcp-mgr-socket'}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-%% @equiv start_link(PoolName, Size, Address, Port, [])
-start_link(PoolName, Size, Address, Port) ->
-  start_link(PoolName, Size, Address, Port, []).
+call_msg(Data) ->
+    gen_statem:call(?MGR, {request, Data}).
 
-%% @doc Creates a pool of TCP connections against the server that
-%%      listen on the given `Address':`Port'.
--spec start_link(
-  atom(),
-  pos_integer(),
-  'tcp-mgr-socket':address(),
-  'tcp-mgr-socket':portnum(),
-  'tcp-mgr-socket':client_options()
-) -> gen:start_ret().
-start_link(PoolName, Size, Address, Port, Options) ->
-  SizeArgs = [{size, Size},
-              {max_overflow, 0}],
-  PoolArgs = [{name, {local, PoolName}},
-              {worker_module, 'tcp-mgr-socket'}] ++ SizeArgs,
-  WorkerArgs = [{address, Address},
-                {port, Port},
-                {options, Options}],
-  poolboy:start_link(PoolArgs, WorkerArgs).
-
-%% @equiv sync_send(PoolName, Msg, Timeout)
-sync_send(PoolName, Msg) ->
-  execute(PoolName, {sync_send, [Msg]}).
-
-%% @doc Sends a message on the current connection and waits until server
-%%      respose arrives. Works as Request/Response.
--spec sync_send(
-  atom(), 'tcp-mgr-socket':message(), timeout()
-) -> 'tcp-mgr-socket':sync_reply().
-sync_send(PoolName, Msg, Timeout) ->
-  execute(PoolName, {sync_send, [Msg, Timeout]}).
-
-%% @equiv send(PoolName, Msg, Timeout)
-send(PoolName, Msg) ->
-  execute(PoolName, {send, [Msg]}).
-
-%% @doc Sends a message on the current connection but returns immediately
-%%      with a request id. Works asynchronously, and if the server sends
-%%      a response back, it is sent directly to the caller process.
--spec send(
-  atom(), 'tcp-mgr-socket':message(), timeout()
-) -> 'tcp-mgr-socket':reply().
-send(PoolName, Msg, Timeout) ->
-  execute(PoolName, {send, [Msg, Timeout]}).
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-
-%% @private
-execute(PoolName, {Cmd, Args}) ->
-  poolboy:transaction(
-    PoolName,
-    fun(Worker) ->
-      apply('tcp-mgr-socket', Cmd, [Worker | Args])
-    end).
+cast_msg(Data) ->
+    gen_statem:cast(?MGR, {request, Data}).
